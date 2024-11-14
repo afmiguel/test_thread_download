6c74abff1c28056b15ac484bf37244315f6e4ac0
afmiguel <afonso.miguel@gmail.com>
2024-11-14 12:07:15 -0300

    Commit inicial

--- src/download.rs
+++ src/download.rs
@@ -3,14 +3,33 @@ use std::io;
 use std::io::Write;
 use std::path::Path;
 
+/// Downloads a file from the specified URL and saves it to the local directory.
+///
+/// # Arguments
+/// * `url` - Base URL where the file is hosted.
+/// * `filename` - Name of the file to be downloaded and saved locally.
 pub fn download_file(url: &str, filename: &str) {
-    print!("Baixando {} ... ", &filename);
-    io::stdout().flush().unwrap();
-    let mut resp = reqwest::blocking::get(format!("{}/{}", url, filename)).expect("request failed");
+    // Print message indicating which file is being downloaded
+    print!("Downloading {} ... ", &filename);
+    io::stdout().flush().unwrap(); // Ensure the message is immediately printed
+
+    // Send a GET request to the server to retrieve the file
+    let mut resp = reqwest::blocking::get(format!("{}/{}", url, filename))
+        .expect("Failed to send request");
+
+    // Define the path where the downloaded file will be saved
     let str_path = format!("downloads/{}", filename);
     let path = Path::new(str_path.as_str());
-    std::fs::create_dir_all(path).expect("failed to create directory");
-    let mut out = File::create(path.join(filename)).expect("failed to create file");
-    io::copy(&mut resp, &mut out).expect("failed to copy content");
+
+    // Create the "downloads" directory if it doesn't already exist
+    std::fs::create_dir_all(path).expect("Failed to create directory");
+
+    // Create a file at the specified path to save the downloaded content
+    let mut out = File::create(path.join(filename)).expect("Failed to create file");
+
+    // Copy the content from the response into the file
+    io::copy(&mut resp, &mut out).expect("Failed to copy content");
+
+    // Print success message
     println!("OK!");
 }
--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,25 @@ use download::download_file;
 const URL: &str = "http://arquivos.afonsomiguel.com";
 
 fn main() {
-    // Download Sequencial
+    let filename_list = vec![
+        "arquivo_1.jpg",
+        "arquivo_2.jpg",
+        "arquivo_3.jpg",
+        "arquivo_4.jpg",
+        "arquivo_5.jpg",
+        "arquivo_6.jpg",
+        "arquivo_7.jpg",
+        "arquivo_8.jpg",
+        "arquivo_9.jpg",
+    ];
+
     let start = Instant::now();
-    for f_cont in 0..=9 {
-        download_file(
-            URL,
-            format!("arquivo_{}.jpg", f_cont).as_str(),
-        );
+    for filename in filename_list {
+        download_file(URL, filename);
     }
     let duration: Duration = start.elapsed();
     println!(
-        "Download em {:.1} segundos",
+        "Downloaded files in {:.1} seconds",
         duration.as_millis() as f32 / 1000.
     );
 }
